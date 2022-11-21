504633db9fc452d7e70f56df5ce29c6cb19f33c6
Pozharskii_Vladimir <baikonur96@yandex.ru>
2022-11-21 16:34:11 +0300

    ver 1.1

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,22 @@ import java.io.File;
 public class Main {
     public static void main(String[] args)
     {
-        String folderPath = "C:\\Users\\Diagnost\\Desktop\\Logs";
+
+        MyThread thread = new MyThread(1);
+        MyThread thread2 = new MyThread(2);
+
+        thread.start();
+        thread2.start();
+
+        String folderPath = "C:\\Users\\Diagnost\\Desktop";
         File file = new File(folderPath);
         System.out.println(file.length());
+        long start = System.currentTimeMillis();
+
         System.out.println(getFolderSize(file));
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
     }
 
     public static long getFolderSize(File folder){
