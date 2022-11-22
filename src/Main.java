ebb8d20534572ee02c19e54774f7e2cc01effae0
Pozharskii_Vladimir <baikonur96@yandex.ru>
2022-11-22 10:27:26 +0300

    ver 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,32 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args)
     {
 
-        MyThread thread = new MyThread(1);
-        MyThread thread2 = new MyThread(2);
-
-        thread.start();
-        thread2.start();
+//        MyThread thread = new MyThread(1);
+//        MyThread thread2 = new MyThread(2);
+//
+//        thread.start();
+//        thread2.start();
 
         String folderPath = "C:\\Users\\Diagnost\\Desktop";
         File file = new File(folderPath);
-        System.out.println(file.length());
-        long start = System.currentTimeMillis();
+//        System.out.println(file.length());
+       long start = System.currentTimeMillis();
 
-        System.out.println(getFolderSize(file));
+       FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+      long size = pool.invoke(calculator);
+        System.out.println(size);
 
-        long duration = System.currentTimeMillis() - start;
-        System.out.println(duration + " ms");
+//        System.out.println(getFolderSize(file));
+
+       long duration = System.currentTimeMillis() - start;
+       System.out.println(duration + " ms");
+
+
     }
 
     public static long getFolderSize(File folder){
